a0d062e52a19e9fcd853ee25dd42f9a980e8da5a
Dan <danhsaw@gmail.com>
2020-04-20 03:34:27 +0800

    Edited ecosystem file for pm2

--- covid19_app/ecosystem.config.js
+++ covid19_app/ecosystem.config.js
@@ -1,18 +1,10 @@
 module.exports = {
     apps : [
       {
-        name: "covid19_webapp",
-        script: "npm",
-        args: "run start",
+        name: "webapp",
+        script: "serve",
+        args: "-s build",
         watch: true,
-        env: {
-          "PORT": 8080,
-          "NODE_ENV": "development"
-        },
-        env_production: {
-          "PORT": 3000,
-          "NODE_ENV": "production",
-        }
       }
     ]
 }
